2967d7687de5e0f4a4adf1a82221c776155f2ad2
Ahmad Danun Hanani <ahmadanun1@gmail.com>
2023-11-17 18:58:53 +0700

    add threads table helper

---
+++ tests/ThreadsTableTestHelper.js
@@ -0,0 +1,31 @@
+/* istanbul ignore file */
+const pool = require('../src/Infrastructures/database/postgres/pool');
+
+const ThreadsTableTestHelper = {
+  async addThread({
+    id = 'thread-123', title = 'dicoding', body = 'Hello World!', owner = 'user-123', date = '2021-08-08T07:22:58.382Z',
+  }) {
+    const query = {
+      text: 'INSERT INTO threads VALUES($1, $2, $3, $4, $5)',
+      values: [id, title, body, owner, date],
+    };
+
+    await pool.query(query);
+  },
+
+  async findThreadById(id) {
+    const query = {
+      text: 'SELECT * FROM threads WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await pool.query(query);
+    return result.rows;
+  },
+
+  async cleanTable() {
+    await pool.query('DELETE FROM threads WHERE 1=1');
+  },
+};
+
+module.exports = ThreadsTableTestHelper;
